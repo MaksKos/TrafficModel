55d049761fabde8b41b6bc63a9aab2173098437a
MaksKos <maks.kostrow@yandex.ru>
2022-10-05 23:52:12 +0300

    realise road <deque>

--- src/traffic_model.py
+++ src/traffic_model.py
@@ -1,34 +1,42 @@
-from turtle import position
 import numpy as np
-
+from collections import deque
 
 class Bus():
 
+    _bus_lenght = 2
+    _vel_max = 3
 
-    def __init__(self) -> None:
+    def __init__(self, line: int, position: int) -> None:
         pass
 
     @staticmethod
-    def initial_position(empty_cells, shape, count):
+    def initial_position(empty_cells, shape: tuple, amount: int):
 
         bus_line = int(0)
-        n_cells = empty_cells[0:shape[1]][1]
-        position = np.sort(np.random.choice(n_cells, size=count, replace=False))
-
-        bus_
-        return bus_position
-
+        buses_cells = empty_cells[empty_cells.T[0] == bus_line]
+        buses_cells = buses_cells[::Bus._bus_lenght]
+        if buses_cells.shape[0] < amount:
+            raise ValueError("No free cells for buses")
+        index_position = np.sort(np.random.choice(buses_cells.T[1], size=amount, replace=False))
+        buses_position = buses_cells[index_position]
+        for i in range(Bus._bus_lenght):
+            empty_cells = np.delete(empty_cells, index_position+i, axis=0)
+        return buses_position, empty_cells
 
 class HumanDriveVehicle():
 
-    def __init__(self) -> None:
+    _vel_max = 3
+
+    def __init__(self, line: int, position: int) -> None:
         pass
 
-    
     @staticmethod
-    def initial_position(empty_cells, shape, count):
-        car_position = None
-        return car_position
+    def initial_position(empty_cells, shape: tuple, amount: int):
+
+        index_position = np.sort(np.random.choice(empty_cells.shape[0], size=amount, replace=False))
+        hdv_position = empty_cells[index_position]
+        empty_cells = np.delete(empty_cells, index_position, axis=0)
+        return hdv_position, empty_cells
 
 class Model():
 
@@ -48,20 +56,47 @@ class Model():
         
         self.n_line = road_parametrs['N_line']
         self.n_cells = road_parametrs['N_cells']
-        empty_cells = list(np.indices((self.n_line, self.n_cells)).transpose(1, 2, 0).reshape(self.n_cells*self.n_line, 2))
-        self.vehicle_position = {}
+        empty_cells = np.indices((self.n_line, self.n_cells), dtype=np.int32).transpose(1, 2, 0).reshape(self.n_cells*self.n_line, 2)
+        vehicle_position = {}
         # position initialization of each vehicle type
         shape = (self.n_line, self.n_cells)
-        for vehicle_type, count in vehicles.items():
-            self.vehicle_position[vehicle_type] = vehicle_type.initial_position(empty_cells, shape, count)
-        self.road_model = self.__make_road_deque()
+        for vehicle_type, amount in vehicles.items():
+            vehicle_position[vehicle_type], empty_cells = vehicle_type.initial_position(empty_cells, shape, amount)
+        self.road_model = self.__make_road_deque(vehicle_position)
 
 
-    def __make_road_deque(self):
-        road_matrix = [[j for j in range(self.n_cells)] for _ in range(self.n_line)]
-        pass
+    def __make_road_deque(self, veh_pos: dict):
+
+        road_matrix = [[None for j in range(self.n_cells)] for _ in range(self.n_line)]
+        for veh_type, positions in veh_pos.items():
+            for position in positions:
+                line = position[0]
+                cell = position[1]
+                road_matrix[line][cell] = veh_type(line, cell)
+
+        road_model = []
+        for line in road_matrix:
+            road_model.append(deque(set(line).remove(None)))
+        return road_model
 
     def step(self):
+
+        self.__get_distance()
+        if self.n_line > 1:
+            for index, line in enumerate(self.road_model):
+                if index == 0:
+                    pass
+                elif index == self.n_line-1:
+                    pass
+                else:
+                    pass
+        self.__get_distance()  
+        #vehicle move
+
+    def __get_distance(self):
+        pass
+     
+    def __line_change(self):
         pass
 
     def model_stabilization(self, n_step):
