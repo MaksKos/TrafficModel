b6e65bb2e34f1e3dfda2d38c79ce79b919304d65
MaksKos <maks.kostrow@yandex.ru>
2022-10-05 18:27:26 +0300

    add new method

--- src/traffic_model.py
+++ src/traffic_model.py
@@ -1,15 +1,21 @@
-from re import A
+from turtle import position
 import numpy as np
 
 
 class Bus():
 
+
     def __init__(self) -> None:
         pass
 
     @staticmethod
-    def initial_position(road_matrix):
-        bus_position = None
+    def initial_position(empty_cells, shape, count):
+
+        bus_line = int(0)
+        n_cells = empty_cells[0:shape[1]][1]
+        position = np.sort(np.random.choice(n_cells, size=count, replace=False))
+
+        bus_
         return bus_position
 
 
@@ -20,7 +26,7 @@ class HumanDriveVehicle():
 
     
     @staticmethod
-    def initial_position(road_matrix):
+    def initial_position(empty_cells, shape, count):
         car_position = None
         return car_position
 
@@ -42,13 +48,30 @@ class Model():
         
         self.n_line = road_parametrs['N_line']
         self.n_cells = road_parametrs['N_cells']
-        self.road_matrix = [[j for j in range(self.n_cells)] for _ in range(self.n_line)]
         empty_cells = list(np.indices((self.n_line, self.n_cells)).transpose(1, 2, 0).reshape(self.n_cells*self.n_line, 2))
         self.vehicle_position = {}
         # position initialization of each vehicle type
-        for vehicle_type in vehicles:
-            self.vehicle_position[vehicle_type] = vehicle_type.initial_position(empty_cells)
+        shape = (self.n_line, self.n_cells)
+        for vehicle_type, count in vehicles.items():
+            self.vehicle_position[vehicle_type] = vehicle_type.initial_position(empty_cells, shape, count)
+        self.road_model = self.__make_road_deque()
 
+
+    def __make_road_deque(self):
+        road_matrix = [[j for j in range(self.n_cells)] for _ in range(self.n_line)]
+        pass
+
+    def step(self):
+        pass
+
+    def model_stabilization(self, n_step):
+        pass
+
+    def model_research(self, n_step):
+        pass
+
+    def get_data(self):
+        pass
 
         
 
