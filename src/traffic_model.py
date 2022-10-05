0c4fc8bbb7868119e9c53830440fa2b72f4e4020
MaksKos <maks.kostrow@yandex.ru>
2022-10-05 11:33:22 +0300

    continue work with init

--- src/traffic_model.py
+++ src/traffic_model.py
@@ -1,3 +1,4 @@
+from re import A
 import numpy as np
 
 
@@ -33,7 +34,7 @@ class Model():
 
         Args:
             road_parametrs (dict): N_line - numbers of line, N_cells - lenght of road 
-            vehicles (dict): <vehicle_type <class>: amount (int)> - dict of vehicle types and amount 
+            vehicles (dict): <vehicle_type <class>: amount (int), ...> - dict of vehicle types and amount 
         """
         for vehicle_type in vehicles:
             if not (vehicle_type in self.__vehicle_type):
@@ -41,11 +42,19 @@ class Model():
         
         self.n_line = road_parametrs['N_line']
         self.n_cells = road_parametrs['N_cells']
-        self.road_matrix = np.zeros((self.n_line, self.n_cells), dtype=np.int8)
+        self.road_matrix = [[j for j in range(self.n_cells)] for _ in range(self.n_line)]
+        empty_cells = list(np.indices((self.n_line, self.n_cells)).transpose(1, 2, 0).reshape(self.n_cells*self.n_line, 2))
         self.vehicle_position = {}
         # position initialization of each vehicle type
         for vehicle_type in vehicles:
-            self.vehicle_position[vehicle_type] = vehicle_type.initial_position(self.road_matrix)
+            self.vehicle_position[vehicle_type] = vehicle_type.initial_position(empty_cells)
+
+
+        
+
+
+
+
         
         
 
