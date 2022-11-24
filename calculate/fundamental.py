d68751509168f8bdd3f142cecc96a5e3133adc36
MaksKos <maks.kostrow@yandex.ru>
2022-11-24 17:31:04 +0300

    calculate

--- calculate/fundamental.py
+++ calculate/fundamental.py
@@ -1,11 +1,9 @@
 import os
-import timeit
 import sys
 
 import numpy as np
-import dask
 import pandas as pd
-from dask.distributed import Client, progress
+from dask.distributed import Client, LocalCluster, progress
 
 modul_path = os.path.abspath(r"..\src\traffic_model")
 if not modul_path in sys.path:
@@ -15,8 +13,8 @@ import traffic_model as model
 
 
 #############################################################
-STEP = 50
-
+STEP = 1
+CORES = 6
 # main settings
 v_max = 5
 # p_cl = 0.8 default
@@ -36,29 +34,22 @@ n_lane = [1, 2]
 if not os.path.isdir(directory):
     os.makedirs(directory)
 
-def calc_data(n_lane):
-
-    client = Client()
-    print( "Workers:" + str(len( list(client.scheduler_info()['workers']) )) )
+def calc_data(n_lane: int, cluster: Client):
 
     road_param = {
         "N_cells": n_cells,
         "N_lane": n_lane,
     }
     fun_step = setting_step(road_param, t_s, t_e, v_max)
-    cars_arr = np.arange(n_cells, step=STEP)[1:5]
-    
-    print(f"Calculate N lane: {n_lane}")
-    results = []
+    cars_arr = cars_arr = np.full(12, 200, dtype=int) #np.arange(n_cells, step=STEP)[1:5]
+    futs = []
     for cars in cars_arr:
-        fun = dask.delayed(fun_step)(cars)
-        results.append(fun)
-
-    results = dask.persist(*results)
-    progress(results)
-    results.compute()
-
-    client.close()
+        work = cluster.submit(fun_step, cars, pure=False)
+        futs.append(work)
+    
+    progress(futs,)
+    print("\n")
+    results = cluster.gather(futs)
     return results
 
 def setting_step(road_set, t_s, t_e, v_max):
@@ -74,11 +65,19 @@ def setting_step(road_set, t_s, t_e, v_max):
 
 def main():
 
+    cluster= LocalCluster(n_workers = CORES, threads_per_worker = 1)
+    client = Client(cluster)
+    print(client)
+
     for n in n_lane:
-        result = calc_data(n)
+        print(f"Calculate N lane: {n}")
+        result = calc_data(n, client)
         tabel = pd.DataFrame(result)
         name = directory + file_name + f"_lane_{n}_v_{v_max}.csv"
         tabel.to_csv(name)
+    
+    client.close()
+    return 0
 
 if __name__ == "__main__":
     
